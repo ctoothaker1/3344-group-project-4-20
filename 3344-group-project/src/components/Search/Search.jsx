def3f09e8498bf6f38e39b04cc150186a25a2b1a
ianxshort <tut42883@temple.edu>
2025-04-02 14:56:54 -0400

    Update Search component with checkbox filter logic

--- 3344-group-project/src/components/Search/Search.jsx
+++ 3344-group-project/src/components/Search/Search.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 const Search = ({ onSearchSubmit }) => {
 
     const [searchQuery, setSearchQuery] = useState('');
+    const[diets,setDiets]= useState([]);
 
     const handleInputChange = (event) => { // change search query as it changes
         setSearchQuery(event.target.value);
@@ -18,9 +19,26 @@ const Search = ({ onSearchSubmit }) => {
         onSearchSubmit(searchQuery);
     }
     };
+    const handleDiets = (event)=>
+    {
+      const { value,checked }=event.target;
+
+
+      if(checked){
+        setDiets(prev=>[...prev,value]); //previous value now becomes [whatever the array was before, the new checked value]
+      }
+      else{
+        setDiets(prev=>prev.filter(diet=>diet!=value)) //prev now equals an array full of diets that aren't the current value *(the user unchecks a diet)
+      }
+
+
+
+    }
+
 
 
   return (
+
     <div className={styles.searchContainer}>
         <form onSubmit={handleSubmit}>
         <label htmlFor="search-box">Search:</label>
@@ -28,7 +46,32 @@ const Search = ({ onSearchSubmit }) => {
         <button type = "submit">Search</button> 
         {/* run search function, redirect to search page*/ }
         </form>
+        <section>
+            <label>
+              <input type="checkbox" value="balanced" checked={diets.includes("balanced")} onChange={handleDiets}/>balanced
+            </label>
+            <label>
+              <input type="checkbox" value="high-fiber" checked={diets.includes("high-fiber")} onChange={handleDiets}/>high-fiber
+            </label>
+            <label>
+              <input type="checkbox" value="high-protein" checked={diets.includes("high-protein")} onChange={handleDiets}/>high-protein
+            </label>
+            <label>
+              <input type="checkbox" value="low-carb" checked={diets.includes("low-carb")} onChange={handleDiets}/>low-carb
+            </label>
+            <label>
+              <input type="checkbox" value="low-fat" checked={diets.includes("low-fat")} onChange={handleDiets}/>low-fat
+            </label>
+            <label>
+              <input type="checkbox" value="low-sodium" checked={diets.includes("low-sodium")} onChange={handleDiets}/>low-sodium
+            </label>
+
+
+
+      </section>
     </div>
+   
+
     
   );
 };
