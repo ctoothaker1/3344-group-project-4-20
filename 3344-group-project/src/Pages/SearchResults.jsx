d768a2c2651607178796a52534464f6cc81711a1
ctoothaker1 <ctoothaker1@gmail.com>
2025-04-03 14:55:46 -0400

    implementing API call for search. Incomplete at the moment.

--- 3344-group-project/src/Pages/SearchResults.jsx
+++ 3344-group-project/src/Pages/SearchResults.jsx
@@ -11,21 +11,50 @@ const SearchResults = () => {
   const { query } = useParams();
   const [results, setResults] = useState([]); // store the search results
   const navigate = useNavigate();
+  
+
+  const [loading, setLoading] = useState(false); //loading state=false by default
+  //API CALL with provided query
+  const fetchSearchResults = async (query) => {
+    setLoading(true); // fetching data, set loading to true
+    try
+    {
+      // this should be the endpoint we created in the server
+      const response = await fetch(`/api/recipes?query=${query}`); 
+      console.log(response);
+      const data = await response.json(); // set data to json API response
+      
+      setResults(data.hits); // hits are the matches
+    }
+    catch(error)
+    {
+      console.error("error fetching recipes from API (in SearchResults.jsx)", error);
+      alert("error fetching recipes from API (in SearchResults.jsx)", error);
+    }
+    finally
+    {
+      setLoading(false)
+    }
+  }
 
   useEffect(() => {
-    //simulate search results until api implemented
-    // replace with the actual api call that returns results
-    //need other functions to get the data from each recipe returned by the api in order to display quick look information
-    const simulatedResults = [
-      'Green salad with avocado',
-      'taco salad',
-      'egg salad',
-      'chicken salad',
-    ].filter((item) =>
-      item.toLowerCase().includes(query.toLowerCase())
-    );
-    setResults(simulatedResults);
-  }, [query]); // this effect runs whenever the query changes
+    fetchSearchResults();
+  },[query]); 
+
+  // useEffect(() => {
+  //   //simulate search results until api implemented
+  //   // replace with the actual api call that returns results
+  //   //need other functions to get the data from each recipe returned by the api in order to display quick look information
+  //   const simulatedResults = [
+  //     'Green salad with avocado',
+  //     'taco salad',
+  //     'egg salad',
+  //     'chicken salad',
+  //   ].filter((item) =>
+  //     item.toLowerCase().includes(query.toLowerCase())
+  //   );
+  //   setResults(simulatedResults);
+  // }, [query]); // this effect runs whenever the query changes
 
     //execute this when search is performed
     const handleSearchSubmit = (searchQuery) => {
--- proxy-server/server.mjs
+++ proxy-server/server.mjs
@@ -7,8 +7,9 @@ dotenv.config(); //pull all environment files from .env into server
 
 const app = express(); // create a new express application
 
-const baseUrl = process.env.BASE_URL_FOR_API;
+const baseUrl = "https://api.edamam.com/api/recipes/v2?type=public";
 const apiKey = process.env.API_KEY;
+const appId = process.env.APP_ID;
 
 app.use(cors(
     //tells the browser that these endpoints are safe, it will not error once we specify our endpoints
@@ -16,28 +17,29 @@ app.use(cors(
         origin: ["http://localhost:5173", "http://localhost:5000"],
         methods: ["GET","POST"],
         allowedHeaders: ["Content-Type", "Authorization"]
-    }
-))
+    }));
 
 // fetch the api using express application
 // this will be specific to our api idk what it should look like
 // all api related tasks should be here to shield it from the frontend
 app.get('/api/recipes', async(request, response) => {
     const {query} = request.query; 
-    const endpoint = `${baseUrl}?q=${query}&api-key=${apiKey}`;
+
+
+    const endpoint = `${baseUrl}&q=${query}&app_id=${appId}&app_key=${apiKey}` //correct
 
     try {
         const response = await fetch(endpoint);
         if (!response.ok){
             // throw an error
-            throw new Error(`error: ${response.status}`);
+            throw new Error(`error (server.mjs): ${response.status}`);
         }
         const data = await response.json();
         response.json(data);
     }
     catch(error){ // handle any errors that are thrown
-        console.error("Error fetching data from API", error);
-        response.status(500).json({error: 'Failed to fetch data'}); // 500 is standard error code from server
+        console.error("Error fetching data from API (server.mjs)", error);
+        response.status(500).json({error: 'Failed to fetch data (server.mjs)'}); // 500 is standard error code from server
 
     }
 }) 
