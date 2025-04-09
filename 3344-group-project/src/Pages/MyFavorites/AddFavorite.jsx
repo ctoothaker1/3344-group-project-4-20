bb2b53c3507376b788e848a1826ce323f9d9fd8d
ianxshort <tut42883@temple.edu>
2025-04-09 16:46:26 -0400

    completed logic to add the recipes on the page, we are going off of colors on this one

--- 3344-group-project/src/Pages/MyFavorites/AddFavorite.jsx
+++ 3344-group-project/src/Pages/MyFavorites/AddFavorite.jsx
@@ -1,8 +1,42 @@
+import React, {useStgate,useEffect} from "react"
+
+const [recipes,setRecipes]
 
 const function AddFavorite()
 {
+    const [recipes,setRecipes]=useState([]);
 
+    const isSelected=recipes.includes(recipes); //determines whether or not the recipe is in their list 
 
+    const handleClick()=>
+    {
+        const updatedArray= isSelected //if the button was previously selected aka the user already added it to the list 
+        ? //we know that the user is trying to remove it from their list (it was already added)
+        recipes.filter(recipe=>recipe!=value)//we want to update the state of the array after removal
+        :  [...recipes,value];
+
+        setRecipes(updatedArray);
+        getList(updatedArray);
+       
+
+    };
+    useEffect(()=>{
+    localStorage.set(recipes)
+ },[recipes] );
+
+        return(
+           <div>
+            <button type="button" onClick={handleClick} style={{
+
+                backgroundColor=isSelected ? "red": "green",
+                color:"green",
+            }}
+            >
+                 Add to List?
+            </button>
+
+           </div>
+        )
 
 
 
