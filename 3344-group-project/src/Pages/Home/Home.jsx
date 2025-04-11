5f015bc63e2bab5c77f1076e22c405cf1900982f
Vivan Dave <123893956+vivan64@users.noreply.github.com>
2025-04-11 14:12:36 -0400

    UpdatedHome.css/jsx

--- 3344-group-project/src/Pages/Home/Home.jsx
+++ 3344-group-project/src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 // this file returns the home page 
 // contains routing functionality for search
 import React from 'react';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 // import siteLogo from '/siteLogo.svg'  -- we should make a logo
 // import './App.css'
-import Search from '../../components/Search/Search'
+import Search from '../../components/Search/Search';
+import './Home.css';
 
 function Home() {
 
@@ -15,30 +16,36 @@ function Home() {
       // redirect to standalone search page
       navigate(`/search/${searchQuery}`);
     }
-  }
+  };
 
   return ( // HOME PAGE LAYOUT
     <>
-        <main style={{ padding: "40px 20px", display: "block" }}>
-          <div style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "20px",
-            flexWrap: "wrap"
-          }}>
-          
-          <h1>this is the homepage</h1>
-          <Search onSearchSubmit={handleSearchSubmit}/> {/* search component*/}
+        <main className="homepage">
+          <div className="homepage-content">
+            <div className="homepage-row">
+              <div className="homepage-left">
+                <h1 className="homepage-title">This is the Homepage</h1>
+                <Search onSearchSubmit={handleSearchSubmit}/> {/* search component*/}
+              </div>
+              <div className="homepage-right">
+                <p className="homepage-description">
+                  Welcome to <strong>Recipe Advisor</strong> - your personalized meal planning/ Recipe Search Assistant!
+                  Easily search for delicious recipes from around  the world, customize your own meal plans, and save
+                  all your favorites all in one place. Whether you're cooking for health, fun, or family, we've got the 
+                  perfect recipe for you. 
+                </p>
+              </div>
+            </div>
+          </div>
           {/* Brief paragraph about our site? */}
           {/* Featured Recipes (breakfasts??)*/}
           {/* Featured Recipes (lunches??)*/}
           {/* Featured Recipes (dinners??)*/}
           {/* Featured Recipes (random??)*/}
           {/* Component for new users: EX button to create your first meal plan, and a button to search*/}
-          </div>
         </main>
     </>
-  )
+  );
 }
 
 export default Home
