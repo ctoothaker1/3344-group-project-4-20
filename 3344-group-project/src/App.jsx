c50935b347e68464c1255a20c326d561279790da
ctoothaker1 <ctoothaker1@gmail.com>
2025-04-12 17:27:43 -0400

    removed unused import from app.jsx

--- 3344-group-project/src/App.jsx
+++ 3344-group-project/src/App.jsx
@@ -6,13 +6,10 @@ import Footer from './components/Footer/Footer'
 import Home from './Pages/Home/Home.jsx'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import SearchResults from './Pages/SearchResults/SearchResults.jsx'
-import FirstSearch from './Pages/FirstSearch/FirstSearch.jsx'
 import MyPlans from './Pages/MyPlans/MyPlans.jsx'
 import Recipe from './Pages/Recipe/Recipe.jsx'
 import MyFavorites from './Pages/MyFavorites/MyFavorites.jsx'
 import MealPlan from './Pages/MealPlan/MealPlan.jsx'
-
-
 
 
 function App() {
