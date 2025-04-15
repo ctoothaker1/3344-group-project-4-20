3dd415f75f984a661068dcf65ae408425ce35239
ctoothaker1 <ctoothaker1@gmail.com>
2025-04-15 11:49:08 -0400

    wrapped App in MealPlansContext in main.jsx so we are able to use mealPlansContext globally.

--- 3344-group-project/src/main.jsx
+++ 3344-group-project/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { FavoritesProvider } from './components/useContext/useContext.jsx'
+import { MealPlansContext } from './components/mealPlansContext/mealPlansContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
-    <FavoritesProvider>
-    <App />
+    <MealPlansContext>
+    <FavoritesProvider> 
+      <App />
     </FavoritesProvider>
+    </MealPlansContext>
   </StrictMode>,
 )
